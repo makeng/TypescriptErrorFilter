680d75a77c8e46d14094e601ef828e5147154097
马兆铿 <810768333@qq.com>
2025-02-23 17:14:08 +0800

    🐞: More accurate bug reports

--- inspect/filter/config.ts
+++ inspect/filter/config.ts
@@ -3,48 +3,53 @@ import { Color, FatalError } from './types';
 
 // 此处增加检查的错误 <错误内容的一部分字符, 标注的格式>
 export const ERROR_GROUP_MAP = new Map<Color, FatalError[]>([
-  [Color.Red, [
-    {
-      title: '代码缺失',
-      txtRegList: [/Cannot find module/i],
-    },
-    {
-      title: '编译错误',
-      txtRegList: [/Parsing error/i],
-    },
-    {
-      title: '重复标识符',
-      txtRegList: [/Duplicate/i],
-    },
-    {
-      title: '没有导出',
-      txtRegList: [/not exported/i, /has no exported member/i],
-    },
-    {
-      title: '调用错误',
-      txtRegList: [/Cannot assign to/i, /can only be/i],
-    },
-  ]],
-  [Color.Orange, [
-    {
-      title: '未初始化',
-      txtRegList: [/has no initializer/i],
-    },
-    {
-      title: '代码不匹配',
-      txtRegList: [/expected/i, /Don't use/i],
-    },
-  ]],
-  [Color.Cyan, [
-    {
-      title: '变量缺失',
-      txtRegList: [/Cannot find name/i, /not exist on/i, /required in/i],
-    },
-    {
-      title: '可能调用错误',
-      txtRegList: [/is possibly/i],
-    },
-  ]],
+  [
+    Color.Red,
+    [
+      {
+        title: '模块丢失',
+        txtRegList: [/not exported/i, /has .+ export.+/i, /Cannot find module/i],
+      },
+      {
+        title: '编译错误',
+        txtRegList: [/Parsing error/i],
+      },
+      {
+        title: '重复标识符',
+        txtRegList: [/Duplicate/i, /multiple properties/i],
+      },
+      {
+        title: '调用错误',
+        txtRegList: [/Cannot assign to/i, /can only be/i],
+      },
+    ],
+  ],
+  [
+    Color.Orange,
+    [
+      {
+        title: '未初始化',
+        txtRegList: [/has no initializer/i],
+      },
+      {
+        title: '代码不匹配',
+        txtRegList: [/expected/i, /Don't use/i],
+      },
+      {
+        title: '变量缺失',
+        txtRegList: [/Cannot find name/i, /not exist on/i, /required in/i],
+      },
+    ],
+  ],
+  [
+    Color.Cyan,
+    [
+      {
+        title: '可能调用错误',
+        txtRegList: [/is possibly/i],
+      },
+    ],
+  ],
 ]);
 
 // 对应的目录
